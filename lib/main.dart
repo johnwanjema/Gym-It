b118075a9034a1fba4d0ba48114681f27be6dea8
john <jonwanjema@gmail.com>
2020-11-14 23:46:47 +0300

    add search bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:the_flutter_way/constants.dart';
 
 void main() => runApp(MyApp());
@@ -8,24 +10,80 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: "Cairo",
-        scaffoldBackgroundColor: kBackgroundColor,
-          textTheme: Theme.of(context).textTheme.apply(displayColor: kTextColor),
-        primarySwatch: Colors.blue,
-      ),
-      home:HomeScreen()
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          fontFamily: "Cairo",
+          scaffoldBackgroundColor: kBackgroundColor,
+          textTheme:
+              Theme.of(context).textTheme.apply(displayColor: kTextColor),
+          primarySwatch: Colors.blue,
+        ),
+        home: HomeScreen());
   }
 }
-
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    var size = MediaQuery.of(context).size; //give height and width of device
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: size.height * .45,
+            decoration: BoxDecoration(
+                color: Color(0xFFF5CEB8),
+                image: DecorationImage(
+                    alignment: Alignment.centerLeft,
+                    image:
+                        AssetImage("assets/images/undraw_pilates_gpdb.png"))),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 52,
+                      width: 52,
+                      decoration: BoxDecoration(
+                          color: Color(0xFFF2BEA1), shape: BoxShape.circle),
+                      child: SvgPicture.asset('assets/icons/menu.svg'),
+                    ),
+                  ),
+                  Text(
+                    'Good Morning of \n John',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline4
+                        .copyWith(fontWeight: FontWeight.w900),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 38),
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(29.5)),
+                    color: Colors.white,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "Search",
+                          border: InputBorder.none,
+                          icon: SvgPicture.asset("assets/icons/search.svg")),
+                    ),
+                  ),
+                 
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
-
